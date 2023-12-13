daa5d231d7a652ff79b4c81dd80845385bf8b03f
André <114594019+andre-c01@users.noreply.github.com>
2023-12-12 23:07:19 -0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+function readingTime(el) {
+  const text = el.innerText;
+  const wpm = 225;
+  const words = text.trim().split(/\s+/).length;
+  const time = Math.ceil(words / wpm);
+  //document.getElementById("time").innerText = time;
+  return time;
+}
+
+body_items = document.getElementsByClassName('entry')
+
+document.getElementById("num_entries").innerText = "0" + body_items.length;
+
+for(var i =0, il = body_items.length;i<il;i++){
+  body_items[i].querySelector('#time').innerText = readingTime(body_items[i].querySelector('.body'));
+  //console.log(body_items[i].querySelector('#time').innerText)
+
+  if (i+1 == il) {
+    body_items[i].focus();
+  }
+}
+
